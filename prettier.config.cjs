df4a8d675908062e4efc7f6a5f7607af3cebbeb0
sebastiandixon <sebastian.dixon@glxdigital.com>
2024-01-01 14:39:50 +0800

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    // plugins: [require.resolve('prettier-plugin-tailwindcss')],
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
